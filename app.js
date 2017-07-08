2bf5c615a62efdacf9c44ae805e50b7bee354808
ShengQiang <qiang_sheng@qq.com>
2017-07-08 19:23:34 +0800

    切换到 koa2框架下

--- app.js
+++ app.js
@@ -1,79 +1,52 @@
-var debug = require('debug')('nina');
-var koa = require('koa');
-//配置文件
-var config = require('./config/config');
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
 
-var app = koa();
-app.use(function *(next){
-    //config 注入中间件，方便调用配置信息
-    if(!this.config){
-        this.config = config;
-    }
-    yield next;
-});
+const index = require('./routes/index')
+const users = require('./routes/users')
 
-//log记录
-var Logger = require('mini-logger');
-var logger = Logger({
-    dir: config.logDir,
-    format: 'YYYY-MM-DD-[{category}][.log]'
-});
+// error handler
+onerror(app)
 
-//router use : this.logger.error(new Error(''))
-app.context.logger = logger;
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
 
-var onerror = require('koa-onerror');
-onerror(app);
+// app.use(views(__dirname + '/views', {
+//   extension: 'pug'
+// }))
 
 //xtemplate对koa的适配
-var xtplApp = require('xtpl/lib/koa');
-//xtemplate模板渲染
-xtplApp(app,{
-    //配置模板目录
-    views: config.viewDir
-});
+// var xtplApp = require('xtpl/lib/koa');
+// //xtemplate模板渲染
+// xtplApp(app,{
+//     //配置模板目录
+//     views: __dirname + '/views'
+// });
+var xtpl = require( './xtpl_koa2/koa2.js' );
+var path = require( 'path' );
+xtpl( app,{
+    views: path.resolve( __dirname, 'views' )
+} );
 
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
 
-var redisStore = require('koa-redis')(config.redis);
-app.redis = redisStore.client;
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
 
-
-
-//session中间件
-var session = require('koa-generic-session');
-
-    var sessionConfig = {
-    store: redisStore,
-    prefix: 'nina:sess:',
-    key: 'nina.sid'
-    };
-
-app.use(session(sessionConfig));
-
-
-//post body 解析
-var bodyParser = require('koa-bodyparser');
-app.use(bodyParser());
-//数据校验
-var validator = require('koa-validator');
-app.use(validator());
-
-//静态文件cache
-var staticCache = require('koa-static-cache');
-var staticDir = config.staticDir;
-app.use(staticCache(staticDir+'/js'));
-app.use(staticCache(staticDir+'/css'));
-
-//路由
-var router = require('koa-router');
-app.use(router(app));
-
-//应用路由
-var appRouter = require('./router/index');
-appRouter(app);
-
-app.listen(config.port);
-console.log('listening on port %s',config.port);
-
-module.exports = app;
-
+module.exports = app

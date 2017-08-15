f446ce02fcd8375db63179263f6cf04c678cb954
ShengQiang <qiang_sheng@qq.com>
2017-08-15 23:29:18 +0800

    修改生产环境数据库

--- config/production.js
+++ config/production.js
@@ -1,10 +1,10 @@
 var config = {
-    // "dbhost":"localhost",
-    // "dbuser":"root",
-    // "dbpwd":"Anhuiqiang851"
-    "dbhost": "172.104.91.83",
-    "dbuser": "admin",
-    "dbpwd": "Anhuiqiang851",
+    "dbhost":"localhost",
+    "dbuser":"root",
+    "dbpwd":"Anhuiqiang85!",
+    // "dbhost": "172.104.91.83",
+    // "dbuser": "admin",
+    // "dbpwd": "Anhuiqiang851",
     'newspaper':"http://localhost:3082/presedocument?url=",//newspaper parse
     'host' : "http://nina.mutangxiaxia.com"
 };

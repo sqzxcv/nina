6fabde9d20c75e99742da3ef8f55327eaf7ecebe
ShengQiang <qiang_sheng@qq.com>
2017-08-16 01:29:12 +0800

    修改生产环节的文本提取接口;修改音频链接地址

--- config/production.js
+++ config/production.js
@@ -5,7 +5,7 @@ var config = {
     // "dbhost": "116.62.195.14",
     // "dbuser": "admin",
     // "dbpwd": "Anhuiqiang85!",
-    'newspaper':"http://localhost:3082/presedocument?url=",//newspaper parse
-    'host' : "http://nina.mutangxiaxia.com"
+    'newspaper':"http://localhost:3082/nina_webCollector/presedocument?url=",//newspaper parse
+    'host' : "http://nina.mutangxiaxia.com:1085"
 };
 module.exports = config;

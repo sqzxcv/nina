42f1cb3c9a406b4444a1f17762db7a4ef55f9af5
ShengQiang <qiang_sheng@qq.com>
2017-08-22 00:17:09 +0800

    nodejs 实现音频合成,拼接

---
+++ src/brain/text2speech_invokePython.js
@@ -0,0 +1,44 @@
+/*global describe, it*/
+// 'use strict'; 
+const request_koa = require("../../common/request_koa.js")
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const path = require('path')
+
+
+const text2speech = async(text) => {
+
+    var ttsPath = path.resolve(__dirname, 'text2speech.py')
+    var cmd = `export WORKON_HOME=~/Envs && source /usr/local/bin/virtualenvwrapper.sh && workon scrapyd_py3.6.1 && python -V && python ${ttsPath} "${text}"`
+    const defaults = {
+        encoding: 'utf-8',
+        timeout: 0,
+        maxBuffer: 2000 * 1024,
+        killSignal: 'SIGTERM',
+        cwd: null,
+        env: null
+    };
+
+    const {
+        stdout,
+        stderr
+    } = await exec(cmd, defaults);
+
+    console.log('stderr:', stderr);
+    if (stderr.length == 0) {
+        console.log('stdout:', stdout);
+        var result = stdout.split('~~~~------follow result:~~~~~~~------')
+        if (result.length == 2) {
+            res = JSON.parse(result[1])['audio']
+            return res
+        } else {
+            console.error("输出结果解析失败")
+            return ""
+        }
+    } else {
+        console.error(stderr)
+        return ""
+    }
+}
+
+module.exports = text2speech;
